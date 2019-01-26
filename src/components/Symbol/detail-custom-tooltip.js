cb1dd7a4fd4880f08e7f769614f8c23ae1909426
Bruno Sanches <obrunosanches@gmail.com>
2019-01-26 12:14:46 -0200

    The menssages were loaded, I made still adjustments in the chart

--- src/components/Symbol/detail-custom-tooltip.js
+++ src/components/Symbol/detail-custom-tooltip.js
@@ -4,7 +4,6 @@ const DetailCustomTooltip = ({ active, payload, external, label }) => {
   if (active) {
     return (
       <div className="area-chart-tooltip">
-        <p>{payload[0].label}</p>
         <p>{`Open: ${payload[0].payload.open}`} </p>
         <p>{`High: ${payload[0].payload.high}`} </p>
         <p>{`Low: ${payload[0].payload.low}`} </p>
